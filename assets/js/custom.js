826d62160dba329f25a113efe563f375cbade310
gianfrancoarone <62422797+gianfrancoarone@users.noreply.github.com>
2021-11-20 12:15:53 +0100

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,15 +1,9 @@
 /**
- * This will be loaded before starting the simulator.
- * If you wish to add custom javascript, 
- * ** make sure to add this line to pxt.json**
+ * Add custom javascript in this files
  * 
- *      "disableTargetTemplateFiles": true
- * 
- * otherwise MakeCode will override your changes.
- * 
- * To register a constrol simmessages, use addSimMessageHandler
+ * To register a constrol simmessages,
+ * use addSimMessageHandler
  */
-
 addSimMessageHandler("web", (data) => {
     switch(data.action) {
         case "open":
